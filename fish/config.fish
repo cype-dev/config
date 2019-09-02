73d8b7355c6eff972b24f0421496247013551f16
cype <mail@mkwa.dev>
2019-09-02 22:48:59 +0200

    Move environment variables to environment.d

--- fish/config.fish
+++ fish/config.fish
@@ -2,16 +2,6 @@ set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME "$HOME/.config"
 
 # Commands/Settings for login-shell
 if status is-login
-    # Use gpg-agent for ssh
-    set -gx SSH_AUTH_SOCK (gpgconf --list-dirs agent-ssh-socket)
-
-    # Environment variables
-    set -gx EDITOR "nvim"
-    set -gx BROWSER "firefox"
-    set -gx TERMINAL "termite"
-    set -gx SXHKD_SHELL "/usr/bin/sh"
-    set fish_user_paths $HOME/.bin $HOME/.cargo/bin
-
     # Set LS_COLORS
     eval (dircolors -c $XDG_CONFIG_HOME/dircolors/dircolors.conf)
 
